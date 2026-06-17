648e227d17f418d5daf4718e0ba15218816c5753
Harsh Pardhi <harshpardhi477@gmail.com>
2026-06-17 15:10:02 +0530

    Intial Commit V2.1

--- app/index.js
+++ app/index.js
@@ -2,3 +2,4 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Secure DevSecOps Monorepo Running!'));
 app.listen(3000, () => console.log('Server running on port 3000'));
+console.log("Hello Devsecops")
